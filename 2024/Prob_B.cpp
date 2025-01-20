e2c6f1bfeb2d34a0afa0ea06a4fb22bd819ebde6
Diewos <153822398+Diewos@users.noreply.github.com>
2025-01-20 18:32:00 +0000

    Problema B 2024 criacao e bases

---
+++ 2024/Prob_B.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+int Meses [12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+int M, D;
+int quanto = 0;
+
+scanf("%d %d", &M, &D);
+
+
+
+printf("%d\n", quanto);
+
+
+}
