16f2d81868df6fdc9c0c9578c6ea7c6ce96fd4df
Diewos <153822398+Diewos@users.noreply.github.com>
2025-01-20 22:34:33 +0000

    porcaria Prob_B 2024

--- 2024/Prob_B.cpp
+++ 2024/Prob_B.cpp
@@ -7,9 +7,34 @@ int Meses [12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 int M, D;
 int quanto = 0;
 
-scanf("%d %d", &M, &D);
+scanf("%d %d", &D, &M);
 
+    if (M == 12 && D < 25)
+        {
+            quanto += 25 - D; 
+        }
+    else if (M == 12 && D > 25)
+        {
+            quanto += Meses[M - 1] - 25;
+            M = 1;
+        }
+    else
+        {
+            quanto = Meses[M-1] - D;
+        }
 
+    for (int i = M - 1; i < 12; i++)
+    {
+        if (i == 11)
+        {
+            break;
+        }
+        else
+        {
+        quanto += Meses[i];
+        }
+    }
+    
 
 printf("%d\n", quanto);
 
