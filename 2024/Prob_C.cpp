d001b3e382e0f6b911faf4afdfc88e80442d2406
Diewos <153822398+Diewos@users.noreply.github.com>
2025-02-03 17:57:47 +0000

    prob c mejoras

--- 2024/Prob_C.cpp
+++ 2024/Prob_C.cpp
@@ -1,23 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <algorithm>
+
+void sort(){
+
+};
 
 int main(){
 
     int quantos_colg;
     int colegas[2][8];
+    int amizade  = 0;
     
     scanf("%d", &quantos_colg);
 
-    for (size_t i = 0; i < 2; i++)
+    for (size_t i = 0; i < quantos_colg; i++)
     {
-        for (size_t j = 0; j < quantos_colg; j++)
+        scanf("%d", &colegas[0][i]);
+    }
+    for (size_t i = 0; i < quantos_colg; i++)
+    {
+        scanf("%d", &colegas[1][i]);
+    }
+    
+
+    for (int i = 0; i < quantos_colg - 1; i++){
+        int temp;
+        if (colegas [0][i] > colegas[0][i+1])
         {
-            scanf("%d", &colegas[i][j]);
+            colegas[0][i] = temp;
+            colegas[0][i] = colegas[0][i+1];
+            colegas[0][i+1] = temp;
+        }
+    }
+    for (int i = 0; i < quantos_colg - 1; i++){
+        int temp;
+        if (colegas [1][i] > colegas[1][i+1])
+        {
+            colegas[1][i] = temp;
+            colegas[1][i] = colegas[1][i+1];
+            colegas[1][i+1] = temp;
         }
     }
 
+        for (int i = 0; i < quantos_colg - 1; i++){
+            if (colegas[0][i] == colegas[0][i + 1]){
+                amizade++;
+                break;
+            } 
+        }
 
+        amizade++;
 
+        for (int i = 0; i < quantos_colg - 1; i++){
+            printf("%d\n", colegas[1][i]);
+            if (colegas[1][i] == colegas[1][i + 1]){
+                amizade--;
+                break;
+            } 
+        }
+        
+        printf("%d", amizade);
 
     return 0;
 }
